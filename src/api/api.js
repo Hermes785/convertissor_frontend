6ef7c6d18281f7e40d054883fd5b1f2b2a1cf0b5
Hermes785 <hermesnguefack785@gmail.com>
2025-02-08 03:17:33 +0100

    add dir for endpoin

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 const Api = () => {
 
     const apiConvertToDocx = 'https://convertisseur-api.mia-services.fr/api/convertPdfToDocx'
-    const apiSendUrl = 'https:convertisseur-api.mia-services.fr/api/urlFile/'
+    const apiSendUrl = 'https://convertisseur-api.mia-services.fr/api/urlFile'
     return { apiConvertToDocx, apiSendUrl };
 
 
